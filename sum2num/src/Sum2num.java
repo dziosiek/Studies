3aab7b3d64fae9e9b42c5f91f84fc9cf73af0cd8
dziosiek <32743473+dziosiek@users.noreply.github.com>
2019-05-05 17:58:02 +0200

    Algorithm created

--- sum2num/src/Sum2num.java
+++ sum2num/src/Sum2num.java
@@ -1,6 +1,22 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
 public class Sum2num {
 
-    public void quickSort(int arr[], int begin, int end) {
+    private static int[] randomArray(int size){
+        /** Method generates random int array with defined size
+         */
+        Random random = new Random();
+        int[] array = new int[size];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(30);
+        }
+        return array;
+
+    }
+
+    private static void quickSort(int arr[], int begin, int end) {
         if (begin < end) {
             int partitionIndex = partition(arr, begin, end);
 
@@ -9,7 +25,7 @@ public class Sum2num {
         }
     }
 
-    private int partition(int arr[], int begin, int end) {
+    private static int partition(int arr[], int begin, int end) {
         int pivot = arr[end];
         int i = (begin - 1);
 
@@ -30,7 +46,39 @@ public class Sum2num {
         return i + 1;
     }
 
+    private static boolean findSum(int[] s1, int[] s2, int x){
+        int left = 0, right = s2.length-1;
+        while(left<s1.length && right>=0){
+            int result = s1[left] + s2[right];
+            if(result==x) {
+                System.out.println("s1["+left+"] = " + s1[left]);
+                System.out.println("s2["+right+"] = " + s2[right]);
+                return true;
+//                return true;
+            }
+            else if(result>x) right--;
+            else left++;
+        }
+        return false;
+
+    }
+
     public static void main(String[] args) {
-        
+        int[] s1 = randomArray(10);
+        int[] s2 = randomArray(20);
+        int x = 10;
+        quickSort(s1,0,s1.length-1);
+        quickSort(s2,0,s2.length-1);
+        System.out.println(Arrays.toString(s1)+" / " + Arrays.toString(s2));
+        System.out.println(findSum(s1, s2, x));
+
+
+
+
+
+
+
+
+
     }
 }
