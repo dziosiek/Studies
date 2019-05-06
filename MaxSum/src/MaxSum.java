0c12cc4c92f7185780fca1bc18070aa4171b64dc
Adam Włosek <a.wlosek@cctechnology.pl>
2019-05-06 13:07:47 +0200

    Modyfing algorithm

--- MaxSum/src/MaxSum.java
+++ MaxSum/src/MaxSum.java
@@ -1,12 +1,16 @@
 public class MaxSum {
     public static void main(String[] args) {
+        int[] array = {1, 20, -20,22,-23,1,25, -4, 1};
+        System.out.println(maxSum(array));
+    }
+    public static int maxSum(int[] array){
         int mainMax = 0, localMax = 0;
-        int[] array = {1, 4, 5, -4, 1};
+
         for (int i = 0; i < array.length; i++) {
             localMax += array[i];
             if(localMax > mainMax) mainMax = localMax;
             else if(localMax<0) localMax = 0;
         }
-        System.out.println(mainMax);
+        return mainMax;
     }
 }
